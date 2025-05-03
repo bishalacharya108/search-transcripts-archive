780495f1d7d1f4249ae637844edcf583e7a0ffa7
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2025-05-04 00:12:02 +0600

    adding daisy ui navbar element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
+        <Navbar></Navbar>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
+      Welcome to Homepage
     </div>
   );
 }
