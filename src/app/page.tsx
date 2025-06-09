c9524a9c5a84ca31a712630ea70c252ad082476b
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2025-06-09 22:53:45 +0600

    add: approved transcript in the homepage (ui changes)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+import DashboardCard from "@/components/DashboardCard";
 import DashboardPage from "./dashboard/page";
 
-export default function Home() {
-  return (
-    <div>
-      Welcome to Homepage
-      {/* <DashboardPage></DashboardPage> */}
-    </div>
-  );
+export default async function Home() {
+
+    const response = await fetch("http://localhost:3000/api/approved", { next: { revalidate: 60 }, credentials: "include" });
+    const { data: transcripts }: { data: TTranscript[] } = await response.json();
+    const sortedTranscripts = [...transcripts].sort(
+        (a, b) => new Date(b.uploadedAt).getTime() - new Date(a.uploadedAt).getTime()
+    );
+    return (
+        <div>
+            <p>Welcome to Homepage
+            </p>
+            <p>Here are the transcripts approved by the admins
+            </p>            
+            {
+                sortedTranscripts.map(transcript => <DashboardCard key={transcript._id} transcript={transcript}></DashboardCard>)
+
+            }
+        </div>
+    );
 }
